88cdc7c904168bff697f90cb61cf6962a14f3b8f
noneno <noneno@kakao.com>
2020-11-05 02:03:27 +0900

    카드리스트의 배열화, 로컬스트리지에 배열 추가
    각 column들의 배열 생성
    
    addCard 함수의 수정
    - 카드가 추가될시 if문을 통해 타겟된 배열에
    	1. 카드의 정보를 push한다.
    	2. push된 배열을 로컬스토리지에 셋팅한다.

--- todolist/js/toDoCardAdd.js
+++ todolist/js/toDoCardAdd.js
@@ -1,6 +1,18 @@
 // 카드 플러스 버튼 가져오기
 const plusBtn = document.getElementsByClassName('add-btn');
 const cardAddForms = document.getElementsByClassName('card-add-form');
+const blackFilter = document.getElementById('filter');
+const popupBtnContainer = document.getElementById('popup-btns');
+
+let beforeLists = [];
+let doingLists = [];
+let afterLists = [];
+
+// 로컬스트리지에 array를 저장하는 함수
+function saveLists(arrayName, TODOS_LOCALSTORAGE) {
+  // object를 string으로 변환해주는 JSON코드
+  localStorage.setItem(arrayName, JSON.stringify(TODOS_LOCALSTORAGE));
+}
 
 // 인자로 선택된 calumn을 받아서 ul값을 조절하는 함수
 // % 보다 더 좋은 방안을 모색중...
@@ -17,7 +29,6 @@ function ulHeightControl(currentColumn) {
 // 플러스 버튼을 눌렀을 때 form 토글
 function plusBtnClickHandler(e) {
   let targetColumn = e.target;
-
   // e.target이 column(section)을 잡을 때까지 타겟을 위임
   while (!targetColumn.classList.contains('column')) {
     targetColumn = targetColumn.parentNode;
@@ -45,15 +56,12 @@ function textareaKeyInputHandler(e) {
 // textarea에 keypress될 때마다 실행될 함수
 function textareaKeypressHandler(e) {
   let addBtn = e.target.parentNode[1];
-
   // 쉬프트키와 엔터키 동시 입력시, 엔터키 입력
   if (e.key == 'Enter' && e.shiftKey) {
     return;
-
     // textarea의 value가 비어 있는데 엔터키 입력시, 엔터키 무시
   } else if (!e.target.value && e.key == 'Enter') {
     e.preventDefault();
-
     // 버튼이 active 되었을 때 엔터키 입력시, cardAdd 함수 실행
   } else if (e.key == 'Enter' && addBtn.classList.contains('active')) {
     e.preventDefault();
@@ -61,12 +69,46 @@ function textareaKeypressHandler(e) {
   }
 }
 
-// list card를 추가하는 함수
+// function deleteCard(e) {
+//   let currentDelBtn = e.target;
+//   while (!currentDelBtn.classList.contains('card-delete-btn')) {
+//     currentDelBtn = currentDelBtn.parentNode;
+//   }
+//   let currentCard = currentDelBtn.parentNode;
+//   let currentUl = currentCard.parentNode;
+
+//   currentUl.removeChild(currentCard);
+// }
+
+function addArrayList(arrayName, currentTextarea) {
+  const newId = arrayName.length + 1;
+  // array에 들어갈 틀
+  const listObj = {
+    text: currentTextarea.value,
+    id: newId,
+  };
+  // 배열 내보내기
+  arrayName.push(listObj);
+}
+
 function addCard(addButton) {
   let currentTextarea = addButton.parentNode.parentNode[0];
-
+  //선택된 column
+  let currentColumn = addButton.parentNode.parentNode.parentNode;
   // 카드를 추가할 선택된 ul
-  let currentColumnUl = addButton.parentNode.parentNode.parentNode.children[2];
+  let currentColumnUl = currentColumn.children[2];
+
+  // column의 id에 따라 배열을 나눠 저장
+  if (currentColumn.id == 'before-items') {
+    addArrayList(beforeLists, currentTextarea);
+    saveLists('beforeLists', beforeLists);
+  } else if (currentColumn.id == 'doing-items') {
+    addArrayList(doingLists, currentTextarea);
+    saveLists('doingLists', doingLists);
+  } else if (currentColumn.id == 'after-items') {
+    addArrayList(afterLists, currentTextarea);
+    saveLists('afterLists', afterLists);
+  }
 
   // 카드 틀 li 생성
   let newCard = document.createElement('li');
@@ -134,6 +176,9 @@ function addCard(addButton) {
   newCard.appendChild(newCardDiv);
   newCard.appendChild(newCardDeleteBtn);
 
+  // delete버튼에 이벤트 추가
+  // newCardDeleteBtn.addEventListener('click', deleteCard);
+
   currentColumnUl.appendChild(newCard);
 
   // textarea value값 초기화
@@ -143,7 +188,6 @@ function addCard(addButton) {
 }
 
 function formBtnClickHandler(e) {
-  console.log(this[1]);
   //active된 add버튼을 눌렀을 시 카드 추가
   if (
     e.target.classList.contains('add-submit-btn') &&
