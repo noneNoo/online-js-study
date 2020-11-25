ea0ff10ba3ddbc2f01bf017396d7d65bc01bfbac
noneno <noneno@kakao.com>
2020-11-26 03:04:03 +0900

    cardgame.js 카드 뒷면에 랜덤색상을 입히는 기능 구현
    random과 concat메서드를 이용해 아예 새로운 랜덤색상 배열을 기존 배열에
    넣어줬다.

--- cardgame/js/cardgame.js
+++ cardgame/js/cardgame.js
@@ -2,8 +2,33 @@ const row = 4;
 const column = 3;
 const mainContainer = document.getElementById('main-container');
 
+const colorLists = [
+  'red',
+  'red',
+  'orange',
+  'orange',
+  'green',
+  'green',
+  'yellow',
+  'yellow',
+  'pink',
+  'pink',
+  'purple',
+  'purple',
+];
+
+let color = [];
+
+for (let i = 0; colorLists.length > 0; i += 1) {
+  // 겹치지 않게 랜덤으로 색상을 뽑아내 배열을 만든다
+  // concat로 새로운 배열 만들어서 기존 배열에 넣어준다
+  color = color.concat(
+    colorLists.splice(Math.floor(Math.random() * colorLists.length), 1)
+  );
+}
+console.log(color);
+
 function settingCard(row, column) {
-  // Element 생성
   for (let i = 0; i < row * column; i++) {
     const card = document.createElement('article');
     const cardInner = document.createElement('div');
@@ -15,6 +40,9 @@ function settingCard(row, column) {
     cardInner.className = 'card-inner';
     cardFront.className = 'card-front';
     cardBack.className = 'card-back';
+
+    // 카드 뒷면에 색상 부여
+    cardBack.style.backgroundColor = color[i];
 
     // Element 조립하기
     cardInner.appendChild(cardFront);
