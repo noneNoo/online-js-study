2c922e721f0f6c07616e0fee2ba38b5da1283f51
noneno <noneno@kakao.com>
2020-12-09 18:54:01 +0900

    minesweeper.js 입력받은 값에따른 기본화면 구현
    가로줄 세로줄 값을 입력받아 실행할 시 dataset에 2차원 배열
    생성함과 동시에 tr태그와 td태그로 html내 구현까지 완료했다.

--- minesweeper/js/minesweeper.js
+++ minesweeper/js/minesweeper.js
@@ -4,11 +4,27 @@ document.querySelector('#exec').addEventListener('click', function (e) {
   const ver = parseInt(document.querySelector('#ver').value);
   const mine = parseInt(document.querySelector('#mine').value);
 
+  const tbody = document.querySelector('#table tbody');
+
   console.log(hor, ver, mine);
 
+  let dataset = [];
+
+  // 2차원 배열로 dataset 만들기
   // table tag 특성상 세로줄(tr) 먼저 만들고
   // 세로줄(tr) 안에 가로줄(td)를 넣는다
-  //   for (let i = 0; i < ver; i++) {
-  //     console.log(i);
-  //   }
+  for (let i = 0; i < ver; i++) {
+    let arr = [];
+    // tr 태그 생성
+    const tr = document.createElement('tr');
+    dataset.push(arr);
+    for (let i = 0; i < hor; i++) {
+      arr.push(1);
+      const td = document.createElement('td');
+      tr.appendChild(td);
+      td.textContent = 1;
+    }
+    tbody.appendChild(tr);
+  }
+  console.log(dataset);
 });
